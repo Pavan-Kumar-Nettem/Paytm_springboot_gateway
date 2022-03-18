0fa85fefba28e689018a6a058d160c594f668e0f
Pavan Kumar <npavankumar918@gmail.com>
2022-03-18 15:55:33 +0530

    first commit

---
+++ src/main/java/com/pavan/spring/paytm/api/SpringPaytmAppApplication.java
@@ -0,0 +1,13 @@
+package com.pavan.spring.paytm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaytmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaytmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavan/spring/paytm/api/SpringPaytmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.spring.paytm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaytmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
